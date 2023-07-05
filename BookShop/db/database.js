a614516d9bacc6ff909cccf385bf42a39bc1bcd0
dnosur <45685967+dnosur@users.noreply.github.com>
2023-07-06 00:05:02 +0300

    Added new data

--- BookShop/db/database.js
+++ BookShop/db/database.js
@@ -1,1 +1,4 @@
-[{"title":"updated1","description":"desc1","author":"aut1","year":1999,"price":30,"id":1}]
+[{"title":"updated1","description":"desc1","author":"aut1","year":1999,"price":30,"id":1},
+{"title":"title2","description":"desc2","author":"aut2","year":2005,"price":45,"id":2},
+{"title":"title3","description":"desc3","author":"aut3","year":2010,"price":20,"id":3},
+{"title":"title4","description":"desc4","author":"aut4","year":2022,"price":55,"id":4}]
