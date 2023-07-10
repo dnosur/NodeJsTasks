8b32cbbfeda5d61e5f1ed5cd52ec668a97764fe8
dnosur <45685967+dnosur@users.noreply.github.com>
2023-07-10 15:11:18 +0300

    Changing methods for http, changed id generation

--- CrudApi1/db/db.js
+++ CrudApi1/db/db.js
@@ -7,7 +7,7 @@ let data = fs.readFileSync(PATH, { encoding: "utf-8", flag: fs.constants.O_CREAT
 if (data.length > 0) data = JSON.parse(data);
 else data = [];
 
-const Create = (obj) => {
+const createData = (obj) => {
     return new Promise((resolve, reject) => {
         data.push(obj);
         fs.writeFile(PATH, JSON.stringify(data), (err) => {
@@ -17,17 +17,18 @@ const Create = (obj) => {
     })
 }
 
-const Read = () => {
+const readData = () => {
     return new Promise((resolve, reject) => {
         (data.length > 0) ? resolve(data) : reject([]);
     })
 }
 
-const Update = (id, obj) => {
+const updateData = (id, obj) => {
     return new Promise((resolve, reject) => {
         if (id >= 0 && id < data.length) {
             obj.id = id;
-            data[id] = obj;
+            data[data.indexOf(data.find(obj => obj.id === id))] = obj;
+            console.log(index);
 
             fs.writeFile(PATH, JSON.stringify(data), (err) => {
                 if (err) reject(err);
@@ -38,10 +39,10 @@ const Update = (id, obj) => {
     })
 }
 
-const Delete = (id) => {
+const deleteData = (id) => {
     return new Promise((resolve, reject) => {
         if (id >= 0 && id < data.length) {
-            data.splice(id);
+            data.splice(data.indexOf(data.find(obj => obj.id === id)));
             fs.writeFile(PATH, JSON.stringify(data), (err) => {
                 if (err) reject(err);
                 else resolve(true);
@@ -52,7 +53,8 @@ const Delete = (id) => {
 }
 
 const GetIndex = () => {
-    return data.length;
+    return (data.length > 0) ? Math.max(...data.map(user => user.id)) : 0;
 }
 
-module.exports = { Create, Read, Update, Delete, GetIndex }
+
+module.exports = { createData, readData, updateData, deleteData, GetIndex }
--- CrudApi1/utils/utils.js
+++ CrudApi1/utils/utils.js
@@ -4,16 +4,13 @@ const validate = (scheme, obj) => {
     return !scheme.validate(obj).error;
 };
 
-const sendAnswer = (promise, res, next, message = null) => {
+const sendAnswer = (promise, res, message = null) => {
     return promise.then(data => {
         if (message) res.end(JSON.stringify(message));
         else res.end(JSON.stringify(data));
-
-        next();
     })
         .catch(err => {
             res.end(JSON.stringify(err));
-            next();
         })
 };
 
