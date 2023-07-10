ecf71e59602167b65b105d63f7c30137cf7f6e1f
dnosur <45685967+dnosur@users.noreply.github.com>
2023-07-10 15:19:23 +0300

    Remove express

--- CrudApi1/index.js
+++ CrudApi1/index.js
@@ -1,31 +1,20 @@
-const express = require("express");
+
 const http = require("http");
 const url = require("url");
 
 const bodyParser = require("body-parser");
 
-const app = express();
-
 const PORT = 8000;
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
 const userRouter = require("./routers/users");
-
-app.use((req,res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader("Content-Type", "application/json");
-    next();
-})
 
 const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader("Content-Type", "application/json");
+
+    bodyParser.urlencoded({extended: false})
+    bodyParser.json()
 
     if(req.url.startsWith("/users")) userRouter(req, res);
 })
